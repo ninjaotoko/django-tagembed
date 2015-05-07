72a4100384681658f04a69cbcf11eaa45d2f1ef5
Xavier <xavier@link-b.com>
2015-05-07 14:43:02 -0300

    add templatetag

---
+++ tagembed/templatetags/tagembed_tags.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+from django import template
+from django.utils.safestring import mark_safe
+from tagembed.tagembed import TedxEmbed, YoutubeEmbed
+
+register = template.Library()
+
+@register.filter
+def parse_tagembed(text):
+
+    ted = TedxEmbed()
+    youtube = YoutubeEmbed()
+
+    parsed_text = ted.parse(text)
+    parsed_text = youtube.parse(parsed_text)
+
+    return mark_safe(parsed_text)
